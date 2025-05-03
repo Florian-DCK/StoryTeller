1bca54d8746c6b07659d249b488f1745d57c7c0d
Florian Donckers <fdonckersf@gmail.com>
2025-05-03 19:51:36 +0200

    try fixing images not working on page user

--- api/views/user.php
+++ api/views/user.php
@@ -15,6 +15,7 @@ $userData = getUserInfosById($db, $_GET['id']);
 <html lang="en">
 
 <head>
+    <base href="/" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/../api/public/global.css">
