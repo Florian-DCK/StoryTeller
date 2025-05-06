d4c21245cdfd4fb504e056c5dfcfda66f0756a88
Florian Donckers <fdonckersf@gmail.com>
2025-05-06 13:50:29 +0200

    fix pas d'image à l'inscription

--- api/controllers/register.php
+++ api/controllers/register.php
@@ -10,13 +10,13 @@ $username = isset($_POST['username']) ? $_POST['username'] : null;
 $email = isset($_POST['email']) ? $_POST['email'] : null;
 $password = isset($_POST['password']) ? $_POST['password'] : null;
 $bio = isset($_POST['bio']) ? $_POST['bio'] : null;
-$avatar = isset($_FILES['avatar']) ? $_FILES['avatar'] : null;
+$avatar = isset($_FILES['avatar']) && $_FILES['avatar']['error'] !== UPLOAD_ERR_NO_FILE ? $_FILES['avatar'] : null;
 
 
-if (!checkValidity($avatar)) {
+if ($avatar !== null && !checkValidity($avatar)) {
     echo "Erreur lors de l'upload de l'image.";
     exit;
-};
+}
 
 
 $mdpHash = password_hash($password, PASSWORD_BCRYPT);
@@ -24,9 +24,11 @@ $mdpHash = password_hash($password, PASSWORD_BCRYPT);
 $result = $authService->logUp($db, $username,$email, $bio, $mdpHash);
 if ($result) {
     $userId = getUserId($db, $username);
-    $avatarPath = saveAvatar($avatar, __DIR__ . '/../ressources/avatars/', $userId);
-    changeAvatar($db, $userId, $avatarPath);
-    $avatarPath = __DIR__ . '/../ressources/avatars/' . basename($avatar['name']);
+    
+    if ($avatar !== null) {
+        $avatarPath = saveAvatar($avatar, __DIR__ . '/../ressources/avatars/', $userId);
+        changeAvatar($db, $userId, $avatarPath);
+    }
     
     header('Location: /auth?success=1');
 } else {
